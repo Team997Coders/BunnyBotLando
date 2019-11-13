8a48d9234ee5cc9f5f0b06c49d0f990c6ff30856
KyroVibe <hunter.barclay18@gmail.com>
2019-11-13 14:55:28 -0800

    Added the new merlin modules

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -5,6 +5,7 @@ import com.kauailabs.navx.frc.AHRS;
 import org.team997coders.spartanlib.commands.UpdateModule;
 import org.team997coders.spartanlib.swerve.SwerveDrive;
 import org.team997coders.spartanlib.swerve.module.HybridModule;
+import org.team997coders.spartanlib.swerve.module.MerlinModule;
 import org.team997coders.spartanlib.swerve.module.SwerveModule;
 
 import edu.wpi.first.wpilibj.SerialPort.Port;
@@ -23,16 +24,16 @@ public class Swerve extends SwerveDrive {
 
     mModules = new HybridModule[4];
 
-    mModules[0] = new HybridModule(0, RobotMap.Ports.AZIMUTH[0], RobotMap.Ports.DRIVE[0], 0,
+    mModules[0] = new MerlinModule(0, RobotMap.Ports.AZIMUTH[0], RobotMap.Ports.DRIVE[0], 0,
         RobotMap.Values.MODULE_FORWARD[0], // Change
         RobotMap.Values.AZIMUTH_P[0], RobotMap.Values.AZIMUTH_I[0], RobotMap.Values.AZIMUTH_D[0]);
-    mModules[1] = new HybridModule(1, RobotMap.Ports.AZIMUTH[1], RobotMap.Ports.DRIVE[1], 1,
+    mModules[1] = new MerlinModule(1, RobotMap.Ports.AZIMUTH[1], RobotMap.Ports.DRIVE[1], 1,
         RobotMap.Values.MODULE_FORWARD[1], // Change
         RobotMap.Values.AZIMUTH_P[1], RobotMap.Values.AZIMUTH_I[1], RobotMap.Values.AZIMUTH_D[1]);
-    mModules[2] = new HybridModule(2, RobotMap.Ports.AZIMUTH[2], RobotMap.Ports.DRIVE[2], 2,
+    mModules[2] = new MerlinModule(2, RobotMap.Ports.AZIMUTH[2], RobotMap.Ports.DRIVE[2], 2,
         RobotMap.Values.MODULE_FORWARD[2], // Change
         RobotMap.Values.AZIMUTH_P[2], RobotMap.Values.AZIMUTH_I[2], RobotMap.Values.AZIMUTH_D[2]);
-    mModules[3] = new HybridModule(3, RobotMap.Ports.AZIMUTH[3], RobotMap.Ports.DRIVE[3], 3,
+    mModules[3] = new MerlinModule(3, RobotMap.Ports.AZIMUTH[3], RobotMap.Ports.DRIVE[3], 3,
         RobotMap.Values.MODULE_FORWARD[3], // Change
         RobotMap.Values.AZIMUTH_P[3], RobotMap.Values.AZIMUTH_I[3], RobotMap.Values.AZIMUTH_D[3]);
 
