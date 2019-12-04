1a1d04dc21b9416e65c8cf6c42408b7cda23f557
TimothyDjang2 <timothydjang@gmail.com>
2019-12-03 17:46:47 -0800

    encoder zeroing

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -9,13 +8,16 @@ import frc.robot.RobotMap;
 import frc.robot.commands.MoveArm;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.SensorCollection;
 
 //subsystem for anything and everything related to the arm on the robot
 //the arm is the big one that moves up and down
 public class Arm extends Subsystem {
 
-  private WPI_TalonSRX armMotor;
+  private TalonSRX armMotor;
+  private SensorCollection sensorCollection;
 
   private DoubleSolenoid grabberSolenoid;
 
@@ -23,21 +25,31 @@ public class Arm extends Subsystem {
   public double peakOutput = .7;
 
   public Arm() {
-    armMotor = new WPI_TalonSRX(RobotMap.Ports.armMotor);
+    armMotor = new TalonSRX(RobotMap.Ports.armMotor);
     armMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
     
     armMotor.configClosedLoopPeakOutput(0, .7);
     grabberSolenoid = new DoubleSolenoid(RobotMap.Ports.grabberSolenoidPort1, RobotMap.Ports.grabberSolenoidPort2); 
+
+    sensorCollection = new SensorCollection(armMotor);
   }
 
   public void setSpeed(double speed) {
     if (Math.abs(speed) > 0.05) {
-      armMotor.set(speed);
+      armMotor.set(ControlMode.PercentOutput, speed);
     }
+  }
+
+  public boolean getTopSwitch() {
+    return sensorCollection.isFwdLimitSwitchClosed();
   }
 
   public double getEncoderTicks(){
     return armMotor.getSelectedSensorPosition(0);
+  }
+
+  public void zeroEncoderTicks() {
+    armMotor.setSelectedSensorPosition(0, 0, 10);
   }
 
   public void grab(){
