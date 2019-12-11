5a20bb6f8a5d8400e74407bccf2cb0ae8813ec7a
TimothyDjang2 <timothydjang@gmail.com>
2019-12-10 17:07:14 -0800

    Made changes from code review to conveyor

--- src/main/java/frc/robot/commands/ConveyorMove.java
+++ src/main/java/frc/robot/commands/ConveyorMove.java
@@ -18,34 +18,23 @@ public class ConveyorMove extends Command {
   private double mSpeed = 0.0;
 
   public ConveyorMove(double speed) {
-    // Use requires() here to declare subsystem dependencies
     requires(Robot.conveyorBelt);
     mSpeed = speed;
   }
 
-  // Called just before this Command runs the first time
-  @Override
-  protected void initialize() {
-  }
-
-  // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
     Robot.conveyorBelt.setSpeed(mSpeed);
   }
 
-  // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() { return false; }
 
-  // Called once after isFinished returns true
   @Override
   protected void end() {
     Robot.conveyorBelt.setSpeed(0.0);
   }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
   @Override
   protected void interrupted() { end(); }
 }
--- src/main/java/frc/robot/subsystems/ConveyorBelt.java
+++ src/main/java/frc/robot/subsystems/ConveyorBelt.java
@@ -1,11 +1,9 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotMap;
 
 
@@ -13,36 +11,22 @@ import frc.robot.RobotMap;
  * The Convyer Belt is attached to the arm and is used to pick up bunnies during the game. 
  */
 public class ConveyorBelt extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
 
   //private. Use a WPI_VictorSPX
   private VictorSPX beltyBoi;
-  //The motor that controls the conveyor belt
   private double maxOutput = 0.7;
 
   public ConveyorBelt(){
     beltyBoi = new VictorSPX(RobotMap.Ports.beltyBoi);
-    beltyBoi.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
-    
     beltyBoi.configPeakOutputForward(maxOutput);
   }
 
   public void setSpeed(double speed) {
     beltyBoi.set(ControlMode.PercentOutput, speed);
   }
-
-  public double getEncoderTicks(){
-    return beltyBoi.getSelectedSensorPosition(0);
-  }
   
-  public void updateSmartDashboard() { 
-    SmartDashboard.putNumber("Conveyor Belt Ticks", getEncoderTicks()); // Jesus please just use the SmartDashboard class
-  }
+  public void updateSmartDashboard() { }
   
   @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
-  }
+  public void initDefaultCommand() { }
 }
